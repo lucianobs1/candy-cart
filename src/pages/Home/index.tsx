b410a8816b0ef1a770c64dd1c523abaf9b01e0db
Luciano Brandao <lucianotetsu@gmail.com>
2022-02-19 22:57:05 -0300

    initial configs added

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+}
+
+export default Home;
