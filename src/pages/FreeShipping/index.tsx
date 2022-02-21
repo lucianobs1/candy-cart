73ef898ad3e787880fc8ea545f8503298dad5ba4
Luciano Brandao <lucianotetsu@gmail.com>
2022-02-21 15:57:40 -0300

    refactored Freeshipping

--- src/pages/FreeShipping/index.tsx
+++ src/pages/FreeShipping/index.tsx
@@ -1,48 +1,19 @@
-import React, { useState } from 'react';
-
-import { Container, CandyContent } from './styles';
+import React from 'react';
+import Cart from '../../components/Cart';
+import { Container } from './styles';
 
 const FreeShipping:React.FC = () => {
-  const [candies, setCandies] = useState<[]>([])
   return (
-    <Container>
-      <header>
-        <h1>Meu carrinho</h1>
-      </header>
-        <CandyContent>
-          <li>
-            <img src='https://www.drogariaminasbrasil.com.br/media/product/029/bombom-garoto-serenata-de-amor-unidade-20g-3b8.jpg' alt="candy" />
-            <div>
-              <strong>Trufa de morango</strong>
-              <p>R$ 1,23</p>
-              <span>R$ 1,11</span>
-            </div>
-          </li>
-          <li>
-            <img src='https://www.drogariaminasbrasil.com.br/media/product/029/bombom-garoto-serenata-de-amor-unidade-20g-3b8.jpg' alt="candy" />
-            <div>
-              <strong>Trufa de morango</strong>
-              <p>R$ 1,23</p>
-              <span>R$ 1,11</span>
-            </div>
-          </li>
-          <li>
-            <img src='https://www.drogariaminasbrasil.com.br/media/product/029/bombom-garoto-serenata-de-amor-unidade-20g-3b8.jpg' alt="candy" />
-            <div>
-              <strong>Trufa de morango</strong>
-              <p>R$ 1,23</p>
-              <span>R$ 1,11</span>
-            </div>
-          </li>
-          <li>
-            <img src='https://www.drogariaminasbrasil.com.br/media/product/029/bombom-garoto-serenata-de-amor-unidade-20g-3b8.jpg' alt="candy" />
-            <div>
-              <strong>Trufa de morango</strong>
-              <p>R$ 1,23</p>
-              <span>R$ 1,11</span>
-            </div>
-          </li>
-        </CandyContent>
+    <>
+      <Container>
+        <header>
+          <h2>Meu carrinho</h2>
+        </header>
+
+        <ul>
+           <Cart />
+           <Cart />
+        </ul>
 
         <div className='payment-container'>
           <div>
@@ -52,7 +23,8 @@ const FreeShipping:React.FC = () => {
         </div>
 
         <button type='button'>Finalizar compra</button>
-    </Container>
+      </Container>
+    </>
   );
 }
 
--- src/pages/FreeShipping/styles.ts
+++ src/pages/FreeShipping/styles.ts
@@ -1,96 +1,36 @@
 import styled from "styled-components";
 
+
 export const Container = styled.div`
-  height: 100%;
-  color: #fff;
-  margin: 0 auto;
-  max-width: 500px;
-  width: 100%;
-
-  header {
-    display: flex;
-    justify-content: center;
-    margin: 0 auto;
-    border-bottom: 1px solid #fff;
-    padding: 40px;
-  }
-
-  button {
-    background-color: #3b74f2;
-    color: #fff;
-    font-weight: bold;
-    border: none;
-    width: 100%;
-    height: 48px;
-    border-radius: 6px;
-    margin: 32px 0;
-
-    transition: filter 0.4s;
-
-    &:hover {
-      filter: brightness(0.4);
-    }
-  }
-
-  .payment-container {
-    display: flex;
-    padding: 24px 0;
-    border-bottom: 1px solid #fff;
-    border-top: 1px solid #fff;
-
-    div {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      max-width: 1128px;
-      width: 100%;
-      margin: 0 auto;
-      padding: 0 32px;
-
-      strong {
-        font-size: 24px;
-      }
-    }
-  }
-`;
-
-export const CandyContent = styled.ul`
   display: flex;
   flex-direction: column;
-  max-width: 1128px;
-  padding: 0 32px;
-  margin: 40px auto;
+  color: #fff;
 
-  li {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 32px;
+  header {
+    text-align: center;
+    padding: 16px 16px;
+    border-bottom: 1px solid #B9BCC7;
+    margin-bottom: 32px;
+  }
 
-    & + li {
-      margin-top: 24px;
+  ul {
+    margin: 0 auto;
+    max-width: 800px;
+    width: 100%;
+
+    li {
+      display: flex;
+      align-items: center;
+      gap: 32px;
+
+      & + li {
+        margin-top: 16px;
+      }
+
+      img {
+        height: 110px;
+        width: 110px;
+      }
     }
-
-    img {
-      height: 120px;
-      width: 120px;
-    }
-
-    strong {
-      display: inline-block;
-      font-size: 18px;
-      margin-bottom: 20px;
-    }
-
-    p {
-      color: #eadbff;
-    }
-
-    span {
-      display: inline-block;
-      font-weight: 700;
-      font-size: 18px;
-      margin-top: 12px;
-    } 
   }
 `;
