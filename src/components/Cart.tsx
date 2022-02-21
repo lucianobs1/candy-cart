d2974710245f59aa17cf643355b7f43e837c1eb1
Luciano Brandao <lucianotetsu@gmail.com>
2022-02-21 15:57:01 -0300

    refactored component Cart

---
+++ src/components/Cart.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Cart: React.FC = () => {
+  return (
+    <li>
+      <img src='https://www.drogariaminasbrasil.com.br/media/product/029/bombom-garoto-serenata-de-amor-unidade-20g-3b8.jpg' alt="candy" />
+      <div>
+        <strong>Trufa de morango</strong>
+        <p>R$ 1,23</p>
+        <span>R$ 1,11</span>
+      </div>
+    </li>
+  )
+}
+
+export default Cart;
